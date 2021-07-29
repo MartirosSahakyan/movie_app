568ed66fe461ab1b046e5cd612c51b8eccf11d80
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-07-29 16:32:01 +0400

    feat(MovieCard.jsx): add MovieCard component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,11 @@
-export default function HomePage (){
-    return(
-        <h1>HOME PAGES</h1>
-    )
+import MoveCard from "../../components/MovieCard";
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>HOME PAGES</h1>
+
+      <MoveCard />
+    </>
+  );
 }
