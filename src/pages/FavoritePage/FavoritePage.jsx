9b342c1851c50fcd20bee4b336c9efdf18a8bce0
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-07-31 19:16:20 +0400

    feat(FavoritePage): filled Favorite page

--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -1,5 +1,27 @@
-export function FavoritePage (){
-    return(
-        <h1>Favorite</h1>
-    )
+import { useState } from "react";
+import MoveCard from "../../components/Movies/MovieCard";
+import styles from "./FavoritePage.module.css";
+
+export function FavoritePage() {
+  const initialState = localStorage.getItem("favorites")
+    ? JSON.parse(localStorage.getItem("favorites"))
+    : "";
+
+  const [movies, setMovie] = useState(initialState);
+  //   console.log(movie.genres);
+  return (
+    <section className={styles.container}>
+      {movies.map((movie) => {
+        return (
+          <MoveCard
+            title={movie.title}
+            imgPath={movie.imgPath}
+            genres={movie.genres}
+            id={movie.id}
+            key={movie.id}
+          />
+        );
+      })}
+    </section>
+  );
 }
