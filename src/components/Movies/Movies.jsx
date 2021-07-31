94c1c59f0d9632738041296768c6d8495e5ca054
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-07-31 15:49:43 +0400

    refactor(Movies.jsx): add Movies component

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import MoveCard from "../../components/MovieCard";
+import { findGenreName } from "../../helper/Helper";
+import { getGenres } from "../../services";
+import styles from "./Movies.module.css";
+
+export default function Movies({ movies, loading }) {
+  const [genres, setGenres] = useState("");
+
+  useEffect(() => {
+    getGenres().then(({ genres }) => {
+      setGenres(genres);
+    });
+  }, []);
+
+  return (
+    <section className={styles.container}>
+      {loading ? (
+        <p>Loading</p>
+      ) : !movies.total_pages ? (
+        <h3>No Such Film</h3>
+      ) : (
+        movies.results.map((movie) => {
+          return (
+            <MoveCard
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              genres={genres ? findGenreName(genres, movie.genre_ids) : []}
+              imgPath={movie.poster_path}
+            />
+          );
+        })
+      )}
+    </section>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
-import MoveCard from "../../components/MovieCard";
-import { getGenres, getMoviesByPage, getMoviesByQuery } from "../../services";
-import styles from "./HomePage.module.css";
+import { getMoviesByPage, getMoviesByQuery } from "../../services";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MovieDetails from "../../components/MovieDetails";
 import Header from "../../components/Header/Header";
-import { findGenreName } from "../../helper/Helper";
+import Movies from "../../components/Movies/Movies";
 
 export default function HomePage() {
   const [movies, setMovies] = useState("");
-  const [genres, setGenres] = useState("");
+
   const [searchQuery, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(true);
   // const [error, setError] = useState(false);
@@ -48,44 +46,16 @@ export default function HomePage() {
     }
   }, [searchQuery]);
 
-  useEffect(() => {
-    getGenres().then(({ genres }) => {
-      setGenres(genres);
-    });
-  }, []);
-
   // console.log(movies);
-  // console.log(genres);
   // console.log(searchQuery);
 
   return (
     <>
       <Router>
         <Header handleSearchInput={handleSearchInput} />
-
         <Switch>
           <Route exact path="/home">
-            <section className={styles.container}>
-              {loading ? (
-                <p>Loading</p>
-              ) : !movies.total_pages ? (
-                <h3>No Such Film</h3>
-              ) : (
-                movies.results.map((movie) => {
-                  return (
-                    <MoveCard
-                      key={movie.id}
-                      id={movie.id}
-                      title={movie.title}
-                      genres={
-                        genres ? findGenreName(genres, movie.genre_ids) : []
-                      }
-                      imgPath={movie.poster_path}
-                    />
-                  );
-                })
-              )}
-            </section>
+            <Movies movies={movies} loading={loading} />
           </Route>
           <Route path="/home/:id" children={<MovieDetails />}></Route>
         </Switch>
