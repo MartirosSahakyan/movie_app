1ff58f2f690a32786924d3801a026d299130d04c
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-08-05 01:19:13 +0400

    feat(Loader.jsx): add size and propTypes

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,10 @@
-import CircularProgress from '@material-ui/core/CircularProgress'
+import PropTypes from "prop-types";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-export default function Loader (){
-    return(
-        <CircularProgress />
-    )
+export default function Loader({ size }) {
+  return <CircularProgress size={size} />;
 }
+
+Loader.propTypes = {
+  size: PropTypes.number,
+};
