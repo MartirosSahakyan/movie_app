ead6ec40a102cd14d9fec68f1ca7369a8425b0ec
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-08-04 17:03:25 +0400

    refactor: change Fetch API by Axios

--- src/service/services.js
+++ src/service/services.js
@@ -1,23 +1,25 @@
+import axios from 'axios'
+
 const API_KEY = "e8e227add2a2e5c168f7c3845928d8db";
 const API_URL = "https://api.themoviedb.org/3/";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 export function getMoviesByPage(page) {
-  return fetch(
+  return axios.get(
     `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
-  ).then((res) => res.json());
+  ).then(res=>res.data)
 }
 
 export function getMovieById(id) {
-  return fetch(
+  return axios.get(
     `${API_URL}movie/${id}?api_key=${API_KEY}&language=en-US`
-  ).then((res) => res.json());
+  ).then(res=>res.data)
 }
 
 export function getGenres() {
-  return fetch(
+  return axios.get(
     `${API_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`
-  ).then((res) => res.json());
+  ).then(res=>res.data)
 }
 
 export function getImgUrl(path) {
@@ -25,7 +27,7 @@ export function getImgUrl(path) {
 }
 
 export function getMoviesByQuery(query) {
-  return fetch(
+  return  axios.get(
     `${API_URL}search/movie?api_key=${API_KEY}&query=${query}`
-  ).then((res) => res.json());
+  ).then(res=>res.data)
 }
