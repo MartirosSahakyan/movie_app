dfc33902a46225cc11ec45dd6ad5ace218891de4
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-08-06 01:12:14 +0400

    refactor(routes.js): extract routes constant

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,30 +6,28 @@ import MovieDetails from "../pages/MovieDetails/MovieDetails";
 import SignUp from "../pages/SignUpPage/SignUp";
 
 export const Routes = {
-    loginPage:{
-        url: '/',
-        component: LoginPage
-    },
-    signUp:{
-        url: '/signUp',
-        component: SignUp
-    },
-    homePage:{
-        url: '/home',
-        component: HomePage
-        
-    },
-    movies:{
-        url: '/home/movies',
-        component: Movies
-    },
-    favoritePage:{
-        url: '/home/favorites',
-        component: FavoritePage
-    },
-    movieDetails:{
-        url: "/home/movies/",
-        component: MovieDetails
-    }   
-        
-}
+  loginPage: {
+    url: "/",
+    component: LoginPage,
+  },
+  signUp: {
+    url: "/signUp",
+    component: SignUp,
+  },
+  homePage: {
+    url: "/home",
+    component: HomePage,
+  },
+  movies: {
+    url: "/home/movies",
+    component: Movies,
+  },
+  favoritePage: {
+    url: "/home/favorites",
+    component: FavoritePage,
+  },
+  movieDetails: {
+    url: "/home/movies/",
+    component: MovieDetails,
+  },
+};
